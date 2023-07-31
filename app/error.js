4a4a5f43bbeb711e6f827f2b5aa75eb979702ebe
SufyanAbbada <sufyan.abada.sa@gmail.com>
2023-08-01 03:06:56 +0500

    Connfigured Project to Deploy on Vercel

---
+++ app/error.js
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Custom404() {
+  return (
+    <div>
+      <p>Sorry , The page you are looking for cant be found</p>
+
+      <p>Try checking your URL</p>
+
+      <h2>
+        This is a <span style={{ color: "red" }}>404 page</span>
+      </h2>
+    </div>
+  );
+}
