fd3fe8c6f29df13915d59e480e3c58ab078ec0ed
SufyanAbbada <sufyan.abada.sa@gmail.com>
2023-08-02 03:13:21 +0500

    Added console to see the output

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -3,5 +3,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const allPosts = await prisma.post.findMany();
+  console.log(allPosts.length);
   return NextResponse.json({ allPosts: allPosts });
 }
